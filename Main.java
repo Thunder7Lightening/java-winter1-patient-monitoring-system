51fff4c962d2d76674c9d9571a03a777a06424ca
Thunder7Lightening <31926495+Thunder7Lightening@users.noreply.github.com>
2018-01-14 12:22:58 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -8,7 +8,8 @@ public class Main
 	
 	public static void main(String[] args) throws Exception
 	{	
-		monitor.setUp(new File("sampleInput"));
+//		monitor.setUp(new File("sampleInput"));
+		monitor.setUp(new File(args[0]));
 		monitor.run();
 		monitor.showDatabase();
 	}
